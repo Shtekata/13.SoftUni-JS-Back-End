ad245ef0308cc937ade15e96b023e019035d2523
Shtekata <gesheval@gmail.com>
2021-01-29 15:56:07 +0200

    Add router middleware

--- 07.Workshop-Express-And-Templating/cubic-project/index.js
+++ 07.Workshop-Express-And-Templating/cubic-project/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import config from './config/config.js';
 import expressConfig from './config/express.js';
+import routes from './router.js';
 
 const app = express();
 expressConfig(app);
+app.use(routes);
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
 
-app.get('/', (req, res) => res.render('home', { layout: false }));
+
