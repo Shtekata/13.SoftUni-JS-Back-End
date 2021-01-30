f668adfaab0845f0a08ef8aed593dc8862c4372e
Shtekata <gesheval@gmail.com>
2021-01-30 13:12:56 +0200

    Add details page

--- 07.Workshop-Express-And-Templating/cubic-project/controllers/productController.js
+++ 07.Workshop-Express-And-Templating/cubic-project/controllers/productController.js
@@ -1,7 +1,8 @@
 import Router from 'express';
 const router = Router();
 
-router.get('/', (req, res) => res.render('home', { title: 'Cubicle' }));
-router.get('/create', (req, res) => res.render('create', { title: 'Create Cube Page' }));
+router.get('/', (req, res) => res.render('products', { title: 'Cubicle' }));
+router.get('/create', (req, res) => res.render('create', { title: 'Create Cube' }));
+router.get('/details/:productId', (req, res) => res.render('details', { title: 'Cube Details' }));
 
 export default router;
