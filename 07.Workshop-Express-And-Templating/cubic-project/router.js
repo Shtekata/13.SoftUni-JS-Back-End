a2cbbf1903cd32da7987ebedb09d27167828f93a
Shtekata <gesheval@gmail.com>
2021-01-30 12:52:01 +0200

    Change about to home controller

--- 07.Workshop-Express-And-Templating/cubic-project/router.js
+++ 07.Workshop-Express-And-Templating/cubic-project/router.js
@@ -1,10 +1,10 @@
 import Router from 'express';
 import productController from './controllers/productController.js';
-import aboutController from './controllers/aboutController.js';
+import homeController from './controllers/homeController.js';
 
 const router = Router();
-router.use('/', productController);
-router.use('/about', aboutController);
+router.use('/', homeController);
+router.use('/products', productController);
 router.get('*', (req, res) => res.render('404', { title: 'Page Not Found' }));
 
 export default router;
