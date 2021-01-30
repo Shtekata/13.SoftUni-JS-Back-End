381bc9f69148936393c676552967ba00bb307e7a
Shtekata <gesheval@gmail.com>
2021-01-30 13:49:35 +0200

    Parse form data

--- 07.Workshop-Express-And-Templating/cubic-project/config/express.js
+++ 07.Workshop-Express-And-Templating/cubic-project/config/express.js
@@ -6,6 +6,8 @@ function setupExpress(app) {
     app.set('view engine', 'hbs');
 
     app.use(express.static('public'));
+
+    app.use(express.urlencoded({ extended: true }));
 }
 
 export default setupExpress;
--- 07.Workshop-Express-And-Templating/cubic-project/controllers/productController.js
+++ 07.Workshop-Express-And-Templating/cubic-project/controllers/productController.js
@@ -2,7 +2,10 @@ import Router from 'express';
 const router = Router();
 
 router.get('/', (req, res) => res.render('products', { title: 'Cubicle' }));
+
 router.get('/create', (req, res) => res.render('create', { title: 'Create Cube' }));
+router.post('/create', (req, res) => res.send(req.body));
+
 router.get('/details/:productId', (req, res) => res.render('details', { title: 'Cube Details' }));
 
 export default router;
