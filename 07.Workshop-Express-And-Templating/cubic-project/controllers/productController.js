8a3648b38c2cceb8fa5094b8917638b8cb1b1695
Shtekata <gesheval@gmail.com>
2021-01-30 13:53:01 +0200

    Redirect to products page

--- 07.Workshop-Express-And-Templating/cubic-project/controllers/productController.js
+++ 07.Workshop-Express-And-Templating/cubic-project/controllers/productController.js
@@ -4,7 +4,7 @@ const router = Router();
 router.get('/', (req, res) => res.render('products', { title: 'Cubicle' }));
 
 router.get('/create', (req, res) => res.render('create', { title: 'Create Cube' }));
-router.post('/create', (req, res) => res.send(req.body));
+router.post('/create', (req, res) => res.redirect('/products'));
 
 router.get('/details/:productId', (req, res) => res.render('details', { title: 'Cube Details' }));
 
