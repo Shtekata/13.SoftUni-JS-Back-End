6f38587a6fb17c0e1761b71b86b0cadc6dd900c4
Shtekata <gesheval@gmail.com>
2021-02-06 23:42:07 +0200

    Add password hash

--- 09.Session-And-Authentication/auth-demo/index.js
+++ 09.Session-And-Authentication/auth-demo/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import config from './config/config.js';
 import expressConfig from './config/express.js';
-import mongoose from 'mongoose';
 import mongooseConfig from './config/mongoose.js';
 import Session from './models/Session.js';
+import bcrypt from 'bcrypt';
 
 const app = express();
 expressConfig(app);
@@ -18,14 +18,31 @@ app.get('/', (req, res) => {
             req.session.user = true;
         })())
         .catch(x => console.log(x.message));
-    const username = req.session?.username;
+    const username = req.session.username;
+    const password = req.session.password;
     const coko = req.session?.coko || '------';
     const car = req.session?.car || 'No car!';
-    res.send(`<h1>Hello ${username || 'n/a'}!</h1> <h1>My coko: ${coko}</h1> <h1>My car: ${car}</h1>`);
+    res.send(`<h1>Hello ${username || 'n/a'}!</h1>
+    <h1>My pass: ${password}</h1>
+    <h1>My coko: ${coko}</h1>
+    <h1>My car: ${car}</h1>`);
 });
 
-app.get('/login/:username', async (req, res) => {
-    const username = req.params?.username;
+app.get('/login/:username/:password', async (req, res) => {
+    const username = req.params.username;
+    
+    const plainTextPassword = req.params.password;
+    const saltRounds = 9;
+    const password = await new Promise((resolve, reject) => {
+        bcrypt.genSalt(saltRounds, (e, x) => {
+        if (e) console.log(e.message);
+        bcrypt.hash(plainTextPassword, x, (e, x) => {
+            if (e) console.log(e.message);
+            resolve(x);
+        });
+    }) 
+    });
+
     if (username) {
         if (!req.session.user) {
             const _id = req.sessionID;
@@ -33,8 +50,12 @@ app.get('/login/:username', async (req, res) => {
             await session.save().catch(x => console.log(x.message))
             req.session.user = true;
         };
-        Session.findByIdAndUpdate(req.sessionID, { username }, { useFindAndModify: false })
-            .then(x => { req.session.username = username; res.send('You have been logged!') })
+        Session.findByIdAndUpdate(req.sessionID, { username, password }, { useFindAndModify: false })
+            .then(x => {
+                req.session.username = username;
+                req.session.password = password;
+                res.send('You have been logged!')
+            })
             .catch(x => console.log(x.message));
     }
     else res.redirect('/');
@@ -76,5 +97,36 @@ app.get('/show', (req, res) => {
 });
 
 app.get('/session', (req, res) => res.send(req.session));
+
+app.get('/compare/:password', (req, res) => {
+    const hash1 = '$2b$09$fPGeLzA0oLN6CkPvssgLJOwkpmjFuLzEoYzVhrW5VQc7JxpaFgXsG';
+    const hash2 = '$2b$09$GM30LYXch/8X2cMpwiEiPutV4hZwhNUrcFmoqzEkf1uGTOUcdM9.2';
+    const hash3 = '$2b$09$aKE0ceIOWq782jefL5bV/OfRekid3/9B4I8Mm/O.sws4eYifffTPe';
+
+    const result1 = new Promise((resolve, reject) => {
+        bcrypt.compare(req.params.password, hash1, (e, x) => {
+            if (e) console.log(e.message);
+            resolve(x);
+        });
+    });
+    const result2 = new Promise((resolve, reject) => {
+        bcrypt.compare(req.params.password, hash2, (e, x) => {
+            if (e) console.log(e.message);
+            resolve(x);
+        });
+    });
+    const result3 = new Promise((resolve, reject) => {
+        bcrypt.compare(req.params.password, hash3, (e, x) => {
+            if (e) console.log(e.message);
+            resolve(x);
+        });
+    });
+
+    Promise.all([result1, result2, result3]).then(x => {
+        const result = [];
+        x.forEach(x => result.push(x));
+        res.send(result);
+    });
+})
 
 app.listen(config.PORT, x => console.log(`Server is listening on port ${config.PORT}...`));
--- 09.Session-And-Authentication/auth-demo/models/Session.js
+++ 09.Session-And-Authentication/auth-demo/models/Session.js
@@ -4,7 +4,8 @@ const sessionScheme = new mongoose.Schema({
     _id: { type: String, required: true, uniq: true },
     username: { type: String,  minlength:3, maxlength: 50 },
     coko: { type: String, minlength:3, maxlength: 50 },
-    car: { type: String, minlength:3, maxlength:50 },
+    car: { type: String, minlength: 3, maxlength: 50 },
+    password: { type: String }
 });
 
 sessionScheme.methods.getInfo = function () { return `My name is ${this.username}` };
